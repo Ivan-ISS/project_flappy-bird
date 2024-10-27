e7efed901df5df01616a90e549120b4960126561
Ivan-ISS <vanya5@inbox.ru>
2024-10-27 22:17:53 +0500

    project_flappy-bird: configured hashing of scss module names

--- config/buildProject/buildLoaders.ts
+++ config/buildProject/buildLoaders.ts
@@ -5,9 +5,37 @@ import { IBuildOptions } from './types/types';
 export function buildLoaders(options: IBuildOptions): ModuleOptions['rules'] {
     const isDev = options.mode === 'development';
 
-    const scssLoader = {
+    const cssLoaderWithModules = {
+        loader: 'css-loader',
+        options: {
+            modules: {
+                localIdentName: isDev ? '[local]--[hash:base64:5]' : '[hash:base64:8]',
+            },
+        },
+    };
+
+    const cssLoaderWithoutModules = {
+        loader: 'css-loader',
+    };
+
+    const scssLoaderGlobal = {
         test: /\.scss$/i,
-        use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            cssLoaderWithoutModules,
+            'sass-loader',
+        ],
+        exclude: /src\/styles\/modules/,
+    };
+
+    const scssLoaderModule = {
+        test: /\.scss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            cssLoaderWithModules,
+            'sass-loader',
+        ],
+        include: /src\/styles\/modules/,
     };
 
     const tsLoader = {
@@ -16,5 +44,5 @@ export function buildLoaders(options: IBuildOptions): ModuleOptions['rules'] {
         exclude: /node_modules/,
     };
 
-    return [scssLoader, tsLoader];
+    return [scssLoaderGlobal, scssLoaderModule, tsLoader];
 }
